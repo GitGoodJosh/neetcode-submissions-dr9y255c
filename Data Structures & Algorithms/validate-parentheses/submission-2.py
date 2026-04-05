f8ab7cc977798b978ae89edfef60505dea2d45de
GitGoodJosh <140815898+GitGoodJosh@users.noreply.github.com>
2026-04-05 21:29:14 +0800

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        key2 =" ({[]})"
+        for i in s:
+            if i in key2[1:4]:
+                stack.append(i)
+            else:
+                if len(stack)>0 and key2[-key2.find(stack[-1])] == i:
+                    stack.pop()
+                else:
+                    return False
+        if len(stack)>0:
+            return False        
+        return True
+                
